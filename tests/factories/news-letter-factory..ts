5c0766e7c0ecdab304f6614dc1d30005295ca3e5
JoaoGMagri <jgmagri@hotmail.com>
2023-05-10 16:56:16 -0300

    refact: test news letter

--- tests/factories/news-letter-factory..ts
+++ tests/factories/news-letter-factory..ts
@@ -1,5 +1,10 @@
 import { newsletter } from "@prisma/client";
 import { prisma } from "../../src/config";
+import { faker } from "@faker-js/faker";
+
+export function generateValidNewsLetterInput() {
+  return { email: faker.internet.email() };
+}
 
 export async function createNewsLetterParticipant(email: string): Promise<newsletter> {
   return await prisma.newsletter.create({
